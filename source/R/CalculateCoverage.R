5a7bf56a87353762654b7896388f8f614b763d22
svogeti <sriharsha_vogeti@intuit.com>
2017-09-11 01:06:34 +0530

    ISSUE-5: Added R script to calculate coverage using Bamsignals

---
+++ source/R/CalculateCoverage.R
@@ -0,0 +1,27 @@
+library(bamsignals)
+library(GenomicRanges)
+
+
+# read commandline arguments
+args <- commandArgs(TRUE)
+bamFile = args[1]
+outputPrefix = args[2]
+windowSize = as.integer(args[3])
+chromosome = args[4]
+chromosomeLength = as.double(args[5])
+
+# construct the query
+query = GRanges(chromosome,IRanges(breakInChunks(chromosomeLength,windowSize)))
+
+# create bincor file
+startPos = as.character(start(query)-1)
+endPos = as.character(end(query))
+fileConnection = file(paste(outputPrefix,"_pCNVD.bincor",sep=""))
+writeLines(paste(startPos,endPos,sep="\t"),con=fileConnection)
+close(fileConnection)
+
+# create bincounts file
+counts = bamCounts(bamFile,query)
+write(counts,file= paste(outputPrefix,"_pCNVD.input",sep=""))
+
+
